ecd6531cbc95eee9a3345baa6d9febdb59b46793
Ines Daniel <i.danie8@gmail.com>
2018-06-27 20:27:03 -0500

    created guessing game with url segment parameters

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -1,6 +1,37 @@
 class Api::GamesController < ApplicationController
   def name_action
-    @name = params["name"].upcase 
+    @name = params["name"]
+    @other_message = params["some_key"]
     render "name_view.json.jbuilder"
   end
+
+  def guess_number_action
+    @message = "hello"
+    render "guess_number_view.json.jbuilder"
+    
+  end
+
+  def segment_param
+    user_input = params["number"].to_i
+    answer = 50
+    if user_input == answer
+      @message = "You win!"
+    elsif user_input < answer
+      @message = "Guess higher"
+    else
+      @message = "Guess lower"
+    end
+    render "segment_param.json.jbuilder"
+  end
+  # def first_letter_action
+  #   @message = params["the name"].upcase
+  #   if parms[0] == "a"
+  #     p @message = "Hey, your name starts with the first letter of the alphabet!"
+  #   else
+  #   end
+  #   render name.json.jbuilder
+  # end
+
 end
+      
+    
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   # verb "/urls" => "namespace/controllers#action"
   namespace :api do 
     get "/name_url" => "games#name_action"
+    get "/guess_number_url" => "games#guess_number_action"
+    # get "/first_letter" => "games#first_letter_action"
+    get "/segment_guessing/:number" => "games#segment_param"
   end 
 end
